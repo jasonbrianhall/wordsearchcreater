88f83863b937f816e7b5545c6c5f26bba67ca1ab
jasonbrianhall@gmail.com <jbhall@fbi.gov>
2023-02-01 10:16:23 -0500

    Latest

--- wordsearch.py
+++ wordsearch.py
@@ -1,8 +1,9 @@
 import random
 import string
+import cups
+import re
 
-
-def create_word_search(words, size=40):
+def create_word_search(words, size=30):
     """Creates a word search from a list of words and returns the grid as a list of lists."""
     grid = [['-' for _ in range(size)] for _ in range(size)]
     
@@ -75,10 +76,43 @@ def print_words_table(words):
     for i, word in enumerate(words):
         print(f'{i+1}. {word}')
 
+def select_printer():
+    """Lists available CUPS printers and allows the user to select one."""
+    conn = cups.Connection()
+    printers = conn.getPrinters()
+    print("Available printers:")
+    for i, printer in enumerate(printers):
+        print("{}: {}".format(i, printer))
+    selected_printer = int(input("Select a printer by its number: "))
+    return list(printers.keys())[selected_printer]
+
+def print_to_selected_printer(grid, printer_id):
+    """Prints the word search puzzle to the selected printer."""
+    conn = cups.Connection()
+    grid_string = '\n'.join([' '.join(row) for row in grid])
+    with open("/tmp/print.txt", "w") as f:
+        f.write(grid_string)
+    conn.printFile(printer_id, "/tmp/print.txt", "Print Job", {})
+
+
 if __name__ == '__main__':
-    words = ['dog', 'cat', 'bird', 'fish']
+    with open("words.txt", "r") as f:
+        words=f.read().split("\n")
+    original_words=words.copy()
+    original_words.remove(original_words[0])
+    original_words.remove("")
+    counter=0
+    for x in words:
+        if counter==0:
+            title=words[counter]
+            words.remove(words[0])
+        else:
+            words[counter] = re.sub(r'[^a-z]+', '', x, flags=re.IGNORECASE).lower()
+        counter+=1
+    words.remove("")
     grid = create_word_search(words)
     replace_dashes(grid)
     print_grid(grid)
     print_words_table(words)
-
+    selected_printer = select_printer()
+    print_to_selected_printer(grid, selected_printer)
